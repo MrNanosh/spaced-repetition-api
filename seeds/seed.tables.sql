4532645d4460b070152b858238064587afeab3c0
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-03 07:25:49 -0800

    first commit: about to pull down from github

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,41 +1,36 @@
 BEGIN;
-
-TRUNCATE
-  "word",
-  "language",
-  "user";
-
+TRUNCATE "word", "language", "user";
 INSERT INTO "user" ("id", "username", "name", "password")
-VALUES
-  (
-    1,
-    'admin',
-    'Dunder Mifflin Admin',
+  VALUES (1, 'admin', 'Dunder Mifflin Admin',
     -- password = "pass"
-    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
-  );
-
+    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG');
 INSERT INTO "language" ("id", "name", "user_id")
-VALUES
-  (1, 'French', 1);
-
+  VALUES (1, 'French', 1);
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
-VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
-
-UPDATE "language" SET head = 1 WHERE id = 1;
-
+  VALUES (1, 1, 'entraine toi', 'practice', 2), (2, 1, 'bonjour', 'hello', 3), (3, 1, 'maison', 'house', 4), (4, 1, 'développeur', 'developer', 5), (5, 1, 'traduire', 'translate', 6), (6, 1, 'incroyable', 'amazing', 7), (7, 1, 'chien', 'dog', 8), (8, 1, 'chat', 'cat', NULL);
+UPDATE
+  "language"
+SET
+  head = 1
+WHERE
+  id = 1;
 -- because we explicitly set the id fields
 -- update the sequencer for future automatic id setting
-SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
-SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
-SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
 
+SELECT
+  setval('word_id_seq', (
+      SELECT
+        MAX(id)
+      FROM "word"));
+SELECT
+  setval('language_id_seq', (
+      SELECT
+        MAX(id)
+      FROM "language"));
+SELECT
+  setval('user_id_seq', (
+      SELECT
+        MAX(id)
+      FROM "user"));
 COMMIT;
+
