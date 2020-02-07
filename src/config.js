9945181355b367c7adf7d24ac877206295fb864a
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-07 07:48:14 -0800

    Revert "deploying"
    This reverts commit ded4086adcc5c4cad52dcbc56f54b903b7cea8d0.

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   PORT: process.env.PORT || 8000,
   NODE_ENV:
     process.env.NODE_ENV ||
     'development',
-  DATABASE_URL:
-    process.env.DATABASE_URL ||
+  DB_URL:
+    process.env.DB_URL ||
     'postgresql://dunder-mifflin@localhost/spaced_repetition',
   JWT_SECRET:
     process.env.JWT_SECRET ||
