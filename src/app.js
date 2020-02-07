8e37aab5e108ff55cef20713c1f8226d0bb348e0
Jon <jonathanjackson424@gmail.com>
2020-02-07 11:18:25 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -13,10 +13,8 @@ const app = express()
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test',
 }))
-app.use(cors)
-  // ({
-//   origin: CLIENT_ORIGIN
-// }))
+app.use(cors())
+
 app.use(helmet())
 
 app.use('/api/auth', authRouter)
