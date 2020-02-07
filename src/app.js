c4a963dfcc242e359aa692dc57a3c5a3bd8167ae
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-07 08:41:53 -0800

    accepted incoming changes

--- src/app.js
+++ src/app.js
@@ -1,29 +1,37 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const errorHandler = require('./middleware/error-handler')
-const authRouter = require('./auth/auth-router')
-const languageRouter = require('./language/language-router')
-const userRouter = require('./user/user-router')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const {
+  NODE_ENV
+} = require('./config');
+const errorHandler = require('./middleware/error-handler');
+const authRouter = require('./auth/auth-router');
+const languageRouter = require('./language/language-router');
+const userRouter = require('./user/user-router');
 
-const app = express()
+const app = express();
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test',
-}))
-app.use(cors())
-<<<<<<< HEAD
-=======
+app.use(
+  morgan(
+    NODE_ENV === 'production'
+      ? 'tiny'
+      : 'common',
+    {
+      skip: () => NODE_ENV === 'test'
+    }
+  )
+);
+app.use(cors());
+app.use(helmet());
 
->>>>>>> 8e37aab5e108ff55cef20713c1f8226d0bb348e0
-app.use(helmet())
+app.use('/api/auth', authRouter);
+app.use(
+  '/api/language',
+  languageRouter
+);
+app.use('/api/user', userRouter);
 
-app.use('/api/auth', authRouter)
-app.use('/api/language', languageRouter)
-app.use('/api/user', userRouter)
+app.use(errorHandler);
 
-app.use(errorHandler)
-
-module.exports = app
+module.exports = app;
